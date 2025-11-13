56f93e91c44ae304cd2cbcdc9f3a831d9c7f83ac
prabhu4168 <81282311+Prabhu970@users.noreply.github.com>
2025-11-13 11:11:34 -0600

    new import

--- frontend/src/routes/auth/LoginForm.tsx
+++ frontend/src/routes/auth/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { supabase } from "../../supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
+
 
 export function LoginForm() {
   const [email, setEmail] = useState("");
