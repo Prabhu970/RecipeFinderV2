9ccf22d3ff8ca0012f5289218f5e529ac77a63f3
prabhu4168 <81282311+Prabhu970@users.noreply.github.com>
2025-11-13 11:03:46 -0600

    Update SignupForm.tsx

--- frontend/src/routes/auth/SignupForm.tsx
+++ frontend/src/routes/auth/SignupForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../../../supabaseClient";
+import { supabase } from "src/lib/supabaseClient";
 
 export function SignupForm() {
   const [username, setUsername] = useState("");
